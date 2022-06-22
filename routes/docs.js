90b7942ab4ae62e3b539ab5a3e039014427bc1d4
NeDinked <mentesnot.ertibu@gmail.com>
2022-06-22 23:25:10 +0300

    restructured for deployment

--- routes/docs.js
+++ routes/docs.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
-const options = require('../docs/swaggerOptions');
+const options = require('../../docs/swaggerOptions');
 
 const specs = swaggerJsdoc(options);
 router.use('/docs', swaggerUi.serve);
